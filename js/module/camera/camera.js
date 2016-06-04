7197fa8503f62083f39bb848931e5fbcd455a4b3
leiming <leiming19877@163.com>
2016-06-04 17:07:31 +0800

    1、文件上传测试。

--- js/module/camera/camera.js
+++ js/module/camera/camera.js
@@ -20,9 +20,9 @@ define(function(require, module, exports) {
 				var reader = gentry.createReader();
 				reader.readEntries(function(list) {
 					pictures.entries = list;
-					window.setTimeout(function(){
+					window.setTimeout(function() {
 						loadingWin.close();
-					},2000);
+					}, 2000);
 				}, function(e) {
 					m.alert("读取录音列表失败：" + e.message);
 				});
@@ -49,10 +49,10 @@ define(function(require, module, exports) {
 		methods: {
 			removePicture: function(e) {
 				var self = this;
-				e.remove(function(entry){
+				e.remove(function(entry) {
 					self.entries.$remove(e);
-				},function(e){
-					m.alert("删除失败，失败原因："+e.message);
+				}, function(e) {
+					m.alert("删除失败，失败原因：" + e.message);
 				});
 			}
 		}
@@ -85,13 +85,83 @@ define(function(require, module, exports) {
 			index: 1
 		});
 	}
-	document.getElementById("caputre-btn").addEventListener("click", capurteImage, false);
-	document.getElementById("clear-pictures-btn").addEventListener("click", function(){
-		gentry.removeRecursively(function(et){
-			pictures.entries = new Array();
-		},
-		function(e){
-			m.alert("失败：" + e.message);
+	//var lfs = null; // 保留上次选择图片列表
+	// 从相册添加文件
+	function pickGallery() {
+		plus.gallery.pick(function(p) {
+			//lfs = p;
+			if (p.files) {
+				for (var i in p.files) {
+					console.log(p.files[i]);
+					plus.io.resolveLocalFileSystemURL(p.files[i], function(entry) {
+						pictures.entries.push(entry);
+					}, function(e) {
+
+					});
+				}
+			} else {
+				plus.io.resolveLocalFileSystemURL(p, function(entry) {
+					pictures.entries.push(entry);
+				}, function(e) {
+					m.alert("读取拍照文件错误：" + e.message);
+				});
+			}
+
+		}, {
+			filename: '_doc/camera/',
+			filter: 'image',
+			multiple: true,
+			maximum: 3,
+			system: false,
+			//selected:lfs,
+			onmaxed: function() {
+				plus.nativeUI.alert("最多选择5张图片。")
+			}
 		});
+	}
+	// 上传文件
+	function upload() {
+		if (pictures.entries.length <= 0) {
+			plus.nativeUI.alert("没有添加上传文件！");
+			return;
+		}
+		var url = "http://192.168.1.103:8080/html5/UploaderServlet";
+		var wt = plus.nativeUI.showWaiting("上传中...");
+		var task = plus.uploader.createUpload(url, {
+				method: "POST"
+			},
+			function(t, status) { //上传完成
+				wt.close();
+				if (status == 200) {
+					plus.nativeUI.alert("上传成功：" + t.responseText);
+				} else {
+					plus.nativeUI.alert("上传失败：" + status);
+				}
+			}
+		);
+		task.addData("client", "HelloH5+");
+		task.addData("uid", getUid());
+		for (var i = 0; i < pictures.entries.length; i++) {
+			var f = pictures.entries[i];
+			task.addFile(f.toLocalURL(), {
+				key: f.name
+			});
+		}
+		task.start();
+	}
+	// 产生一个随机数
+	function getUid() {
+		return Math.floor(Math.random() * 100000000 + 10000000).toString();
+	}
+	document.getElementById("caputre-btn").addEventListener("click", capurteImage, false);
+	document.getElementById("gallery-btn").addEventListener("click", pickGallery, false);
+	document.getElementById("upload-pictures-btn").addEventListener("click", upload, false);
+	document.getElementById("clear-pictures-btn").addEventListener("click", function() {
+		gentry.removeRecursively(function(et) {
+				pictures.entries = new Array();
+			},
+			function(e) {
+				m.alert("失败：" + e.message);
+			});
 	}, false);
 });
